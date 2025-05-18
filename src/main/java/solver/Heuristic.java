afd41e8729621df3b50ea4d4eaa15008901d585a
unknown <naufarrelzhafifabhista@gmail.com>
2025-05-18 17:30:55 +0700

    feat: added solver package abstraction

---
+++ src/main/java/solver/Heuristic.java
@@ -0,0 +1,24 @@
+package solver;
+
+import logic.GameState;
+
+/**
+ * Interface for heuristic functions used by search algorithms.
+ * Heuristic functions estimate the cost from a state to the goal state.
+ */
+public interface Heuristic {
+    /**
+     * Calculates the heuristic value for a given game state.
+     * 
+     * @param state The game state to evaluate
+     * @return The estimated distance/cost to goal (lower is better)
+     */
+    int calculate(GameState state);
+    
+    /**
+     * Gets the name of this heuristic for reporting purposes.
+     * 
+     * @return The heuristic name
+     */
+    String getName();
+}
---
+++ src/main/java/solver/InformedSolver.java
@@ -0,0 +1,44 @@
+package solver;
+
+/**
+ * Abstract base class for informed search algorithms that use heuristics.
+ * This extends the general Solver with heuristic-specific functionality.
+ */
+public abstract class InformedSolver extends Solver {
+    
+  protected Heuristic heuristic;
+  
+  /**
+   * Constructor with specified heuristic.
+   * 
+   * @param heuristic The heuristic to use for search
+   */
+  public InformedSolver(Heuristic heuristic) {
+      super();
+      this.heuristic = heuristic;
+  }
+  
+  /**
+   * Gets the heuristic used by this solver.
+   * 
+   * @return The heuristic
+   */
+  public Heuristic getHeuristic() {
+      return heuristic;
+  }
+  
+  /**
+   * Sets a new heuristic for this solver.
+   * 
+   * @param heuristic The new heuristic to use
+   */
+  public void setHeuristic(Heuristic heuristic) {
+    this.heuristic = heuristic;
+  }
+  
+  @Override
+  public void printStatistics() {
+    super.printStatistics();
+    System.out.println("Heuristik: " + heuristic.getName());
+  }
+}
